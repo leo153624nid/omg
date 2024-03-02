2ccebd1b691aed9f12cb6e8df1375b1632d2116e
Aleksey Chaykin <chaikinalexey@letmecode.org>
2024-03-02 11:44:14 +0300

    Initial Commit

---
+++ omg/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  omg
+//
+//  Created by Andrey Buksha2 on 02.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ omg/omgApp.swift
@@ -0,0 +1,17 @@
+//
+//  omgApp.swift
+//  omg
+//
+//  Created by Andrey Buksha2 on 02.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct omgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
